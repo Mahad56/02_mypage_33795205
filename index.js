8f328140b8ada944459ec59514aad5c4dc4044a1
Mahad56 <mosma003@campus.goldsmiths.ac.uk>
2025-10-07 11:19:27 +0100

    Initial version

---
+++ index.js
@@ -0,0 +1,23 @@
+const http = require("http");
+const port = 8000;
+
+http.createServer(function(req, res) {
+    console.log(req); 
+
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(`
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <title>My Page</title>
+            </head>
+            <body>
+                <h1>Hello, I'm Mahad!</h1>
+                <h2>About Me</h2>
+                <p>I am a Comp Sci student at Goldsmiths.</p>
+            </body>
+        </html>
+    `);
+}).listen(port, function() {
+    console.log(`Node server is running on port ${port}...`);
+});
